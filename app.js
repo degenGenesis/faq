97bb9ecb361d8d2482001236294f910ee6121f54
Trevor Jones <degengenesis@protonmail.com>
2022-10-03 14:55:11 -0700

    Add: alternative logic

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-//using selectors inside the element
-// traversing the dom
+/* <--Using selectors inside the element--> */
+const questions = document.querySelectorAll(".question");
 
-const btns = document.querySelectorAll('.question-btn');
-
-btns.forEach((btn) =>{
-  btn.addEventListener('click', (e) => {
-    const question = e.currentTarget.parentElement.parentElement;
+questions.forEach((question) => {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener('click', () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
     question.classList.toggle('show-text');
   });
 });
+
+/* <--Traversing the DOM--> */
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach((btn) =>{
+//   btn.addEventListener('click', (e) => {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
